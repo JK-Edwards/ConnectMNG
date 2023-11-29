ac6d4680149d70b6c989662f6158743c40127b9a
jkedwards <jamese8@uw.edu>
2023-11-28 20:30:30 -0800

    Added header and footer with social media link

--- src/App.js
+++ src/App.js
@@ -10,24 +10,37 @@ import Donate from './pages/donate';
 import Events from './pages/events';
 import Resources from './pages/resources';
 import Blogs from './pages/blogs';
+import instaLogo from "./Images/insta.avif";
 
 function App() {
 	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path='/index' element={<Home />} />
-				<Route path='/programs' element={<Programs />} />
-				<Route path='/volunteer' element={<Volunteer />} />
-				<Route path='/donate' element={<Donate />} />
-				<Route path='/events' element={<Events />} />
-				<Route path='/resources' element={<Resources />} />
-				<Route path='/blogs' element={<Blogs />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/contact' element={<Contact />} />
-			</Routes>
-		</Router>
+		<div>
+			<div>
+				<a href="https://www.instagram.com/">
+					<img src={instaLogo}/>
+				</a>
+			</div>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path='/index' element={<Home />} />
+					<Route path='/programs' element={<Programs />} />
+					<Route path='/volunteer' element={<Volunteer />} />
+					<Route path='/donate' element={<Donate />} />
+					<Route path='/events' element={<Events />} />
+					<Route path='/resources' element={<Resources />} />
+					<Route path='/blogs' element={<Blogs />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</Router>
+			<div>
+				<a href="https://www.instagram.com/">
+					<img src={instaLogo}/>
+				</a>
+			</div>
+		</div>
 	);
 }
 
