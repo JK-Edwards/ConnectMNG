cd235c2abe4b26c1a9e7f31a7472d72cd00a3ffe
jkedwards <jamese8@uw.edu>
2023-12-04 18:59:01 -0800

    Created icon for language change button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,9 @@ import "./Header.css";
 export default function Header() {
     return (
         <div className={"headerContainer"}>
+            <div className={"languageIcon"}>
+                ENG
+            </div>
             <a href="https://www.instagram.com/anuujinii" target={"_blank"} rel={"noreferrer"}>
                 <img src={instaLogo} className={"icon"} alt={"Instagram"}/>
             </a>
