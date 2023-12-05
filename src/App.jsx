358eafc2857cdb550943dfac0245bdd7605806d4
jkedwards <jamese8@uw.edu>
2023-12-05 00:01:23 -0800

    Reverted navigation to use routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
+import About from "./pages/About/About.jsx";
+import Blogs from "./pages/Blogs/Blogs.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
 import Navigation from "./components/Navigation/Navigation.jsx";
-import PageRenderer from "./components/PageRenderer/PageRenderer.jsx";
-import React, { useState } from "react";
+import Programs from "./pages/Programs/Programs.jsx";
+import React from "react";
+import Resources from "./pages/Resources/Resources.jsx";
+import Volunteer from "./pages/Volunteer/Volunteer.jsx";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
-	const [activeIndex, setActiveIndex] = useState(0);
-
 	const [isEnglish, setIsEnglish] = useState(true);
 
 	return (
-		<div className={"app"}>
-			<Header isEnglish={isEnglish} onClick={setIsEnglish}/>
-			<Navigation activeIndex={activeIndex} onClick={setActiveIndex}/>
-			<PageRenderer activeIndex={activeIndex}/>
-			<Footer/>
-		</div>
+		<Router>
+			<div className={"app"}>
+				<Header isEnglish={isEnglish} onClick={setIsEnglish} />
+				<Navigation />
+				<Routes>
+					<Route path="*" element={<Home />} />
+					<Route path="/programs" element={<Programs />} />
+					<Route path="/get-involved" element={<Volunteer />} />
+					<Route path="/blogs" element={<Blogs />} />
+					<Route path="/about-us" element={<About />} />
+					<Route path="/resources" element={<Resources />} />
+					<Route path="/contact-us" element={<Contact />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
 	);
 };
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,32 +2,38 @@ import classNames from "classnames"
 import logo from "../../images/logo.png";
 import React from "react";
 
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import "./Navigation.css";
 
-export default function Navigation({activeIndex, onClick}) {
+export default function Navigation() {
+	const [activeIndex, setActiveIndex] = useState(0);
 	const contactButtonClasses = classNames("button", "contact");
 
 	return (
 		<div className={"navigationContainer"}>
-			<img src={logo} alt={"Connect MNG Logo"} className={"logo"} onClick={() => onClick(0)}/>
-			<Tab text={"Programs"} className={"button"} isActive={activeIndex === 1} onClick={() => onClick(1)}/>
-			<Tab text={"Get Involved"} className={"button"} isActive={activeIndex === 2} onClick={() => onClick(2)} />
-			<Tab text={"Blogs"} className={"button"} isActive={activeIndex === 3} onClick={() => onClick(3)} />
-			<Tab text={"About Us"} className={"button"} isActive={activeIndex === 4} onClick={() => onClick(4)} />
-			<Tab text={"Resources"} className={"button"} isActive={activeIndex === 5} onClick={() => onClick(5)} />
-			<Tab text={"Contact Us"} className={contactButtonClasses} isActive={activeIndex === 6} onClick={() => onClick(6)} />
-		</div>
+			<Link to="/" onClick={() => setActiveIndex(0)}>
+				<img src={logo} alt={"Connect MNG Logo"} className={"logo"} />
+			</Link>
+			<NavLink to="/programs" text="Programs" className={"button"} isActive={activeIndex === 1} onClick={() => setActiveIndex(1)}/>
+			<NavLink to="/get-involved" text="Get Involved" className={"button"} isActive={activeIndex === 2} onClick={() => setActiveIndex(2)}/>
+			<NavLink to="/blogs" text="Blogs" className={"button"} isActive={activeIndex === 3} onClick={() => setActiveIndex(3)}/>
+			<NavLink to="/about-us" text="About Us" className={"button"} isActive={activeIndex === 4} onClick={() => setActiveIndex(4)}/>
+			<NavLink to="/resources" text="Resources" className={"button"} isActive={activeIndex === 5} onClick={() => setActiveIndex(5)}/>
+			<NavLink to="/contact-us" text="Contact Us" className={contactButtonClasses} isActive={activeIndex === 6} onClick={() => setActiveIndex(6)}/>
+    	</div>
 	);
 };
 
-function Tab({text, isActive, onClick, className}) {
+function NavLink({to, text, isActive, className, onClick}) {
 	if (isActive && (className !== classNames("button", "contact"))) {
 		className = classNames(className, "active");
-	}
+	};
 
 	return (
-		<div onClick={onClick} className={className}>
+		<Link to={to} className={className} onClick={onClick}>
 			{text}
-		</div>
+		</Link>
 	);
 };
