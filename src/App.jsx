903feaf91edce81d52526831fc652144cf60b414
jkedwards <jamese8@uw.edu>
2023-12-05 17:28:46 -0800

    Added language change to navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 		<Router>
 			<div className={"app"}>
 				<Header isEnglish={isEnglish} setIsEnglish={setIsEnglish} />
-				<Navigation />
+				<Navigation isEnglish={isEnglish}/>
 				<Routes>
 					<Route path="*" element={<Home />} />
 					<Route path="/programs" element={<Programs />} />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,21 +7,28 @@ import { useState } from "react";
 
 import "./Navigation.css";
 
-export default function Navigation() {
+export default function Navigation({isEnglish}) {
 	const [activeIndex, setActiveIndex] = useState(0);
 	const contactButtonClasses = classNames("button", "contact");
+
+	const programsText = isEnglish ? "Programs" : "Төслүүд";
+	const volunteerText = isEnglish ? "Get Involved": "Таны Оролцоо";
+	const blogsText = isEnglish ? "Blogs": "Блог";
+	const aboutText = isEnglish ? "About Us": "Бидний Тухай";
+	const resourcesText = isEnglish ? "Resources": "Нэмэлт Мэдээлэл";
+	const contactText = isEnglish ? "Contact Us" : "Бидэнтэй Холбогдох";
 
 	return (
 		<div className={"navigationContainer"}>
 			<Link to="/" onClick={() => setActiveIndex(0)}>
 				<img src={logo} alt={"Connect MNG Logo"} className={"logo"} />
 			</Link>
-			<NavLink to="/programs" text="Programs" className={"button"} isActive={activeIndex === 1} onClick={() => setActiveIndex(1)}/>
-			<NavLink to="/get-involved" text="Get Involved" className={"button"} isActive={activeIndex === 2} onClick={() => setActiveIndex(2)}/>
-			<NavLink to="/blogs" text="Blogs" className={"button"} isActive={activeIndex === 3} onClick={() => setActiveIndex(3)}/>
-			<NavLink to="/about-us" text="About Us" className={"button"} isActive={activeIndex === 4} onClick={() => setActiveIndex(4)}/>
-			<NavLink to="/resources" text="Resources" className={"button"} isActive={activeIndex === 5} onClick={() => setActiveIndex(5)}/>
-			<NavLink to="/contact-us" text="Contact Us" className={contactButtonClasses} isActive={activeIndex === 6} onClick={() => setActiveIndex(6)}/>
+			<NavLink to={"/programs"} text={programsText} className={"button"} isActive={activeIndex === 1} onClick={() => setActiveIndex(1)}/>
+			<NavLink to={"/get-involved"} text={volunteerText} className={"button"} isActive={activeIndex === 2} onClick={() => setActiveIndex(2)}/>
+			<NavLink to={"/blogs"} text={blogsText} className={"button"} isActive={activeIndex === 3} onClick={() => setActiveIndex(3)}/>
+			<NavLink to={"/about-us"} text={aboutText} className={"button"} isActive={activeIndex === 4} onClick={() => setActiveIndex(4)}/>
+			<NavLink to={"/resources"} text={resourcesText} className={"button"} isActive={activeIndex === 5} onClick={() => setActiveIndex(5)}/>
+			<NavLink to={"/contact-us"} text={contactText} className={contactButtonClasses} isActive={activeIndex === 6} onClick={() => setActiveIndex(6)}/>
     	</div>
 	);
 };
