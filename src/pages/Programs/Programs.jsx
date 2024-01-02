c5febb338c9e1ce7aa4562641d0cc9ed27daba1c
jkedwards <jamese8@uw.edu>
2024-01-02 15:54:24 -0800

    Finished styling programs page

--- src/pages/Programs/Programs.jsx
+++ src/pages/Programs/Programs.jsx
@@ -15,23 +15,25 @@ export default function Programs() {
 			<div className={"missionTextContainer"}>
 				<h1>{intl.formatMessage({id: "missionAndImpact"})}</h1>
 				<p>{intl.formatMessage({id: "mission"})}</p>
-				<h1>{intl.formatMessage({id: "programs"})}</h1>
 			</div>
-			<div className={"programGallery"}>
-				<div className="program">
-					<img className={"programImage"} src={prog1} alt={"Career Development Program"}></img>
-					<h3>{intl.formatMessage({id: "careerDevelopment"})}</h3>
-					<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
-				</div>
-				<div className="program">
-					<img className={"programImage"} src={prog2} alt={"Mentorship Program"}></img>
-					<h3>{intl.formatMessage({id: "mentorship"})}</h3>
-					<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
-				</div>
-				<div className="program">
-					<img className={"programImage"} src={prog3} alt={"Networking Program"}></img>
-					<h3>{intl.formatMessage({id: "networking"})}</h3>
-					<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
+			<div className={"programGalleryContainer"}>
+				<h1>{intl.formatMessage({id: "programs"})}</h1>
+				<div className={"programGallery"}>
+					<div className="program">
+						<img className={"programImage"} src={prog1} alt={"Career Development Program"}></img>
+						<h3>{intl.formatMessage({id: "careerDevelopment"})}</h3>
+						<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
+					</div>
+					<div className="program">
+						<img className={"programImage"} src={prog2} alt={"Mentorship Program"}></img>
+						<h3>{intl.formatMessage({id: "mentorship"})}</h3>
+						<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
+					</div>
+					<div className="program">
+						<img className={"programImage"} src={prog3} alt={"Networking Program"}></img>
+						<h3>{intl.formatMessage({id: "networking"})}</h3>
+						<a className={"learnMoreLink"} href={"https://www.google.co.uk/"}>{intl.formatMessage({id: "learnMore"})}</a>
+					</div>
 				</div>
 			</div>
 		</div>
