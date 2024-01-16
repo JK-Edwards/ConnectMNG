300823b3f675cee776a7908bff5f0e437ede8ff8
jkedwards <jamese8@uw.edu>
2024-01-15 16:32:13 -0800

    Removed size prop from profiles

--- src/components/BoardMemberGallery/BoardMemberGallery.jsx
+++ src/components/BoardMemberGallery/BoardMemberGallery.jsx
@@ -3,14 +3,12 @@ import React from "react";
 
 import "./BoardMemberGallery.css";
 
-function Profile({person, size}) {
+function Profile({person }) {
     return (
       <div className={"profile"}>
         <img
             src={person.imageId}
             alt={person.name}
-            width={size}
-            height={size}
         />
         <h3>
           {person.name}
@@ -31,38 +29,31 @@ export default function BoardMemberGallery() {
           <Profile person={{name: "Anuujin Ider",
                             imageId: anunu,
                             description: "President",
-                            linkedIn: "https://www.linkedin.com/in/anuujini/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/anuujini/"}} />
           <Profile person={{name: "Saruul-Erdene Ganbayar",
                             imageId: anunu,
                             description: "Vice President",
-                            linkedIn: "https://www.linkedin.com/in/saruul-erdene/"}}
-                    size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/saruul-erdene/"}} />
           <Profile person={{name: "Selenge Ulam-Urnukh",
                             imageId: anunu,
                             description: "Program Director",
-                            linkedIn: "https://www.linkedin.com/in/selengeuo/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/selengeuo/"}} />
           <Profile person={{name: "Badmaarag Batjargal",
                             imageId: anunu,
                             description: "Marketing Director",
-                            linkedIn: "https://www.linkedin.com/in/badmaarag-batjargal-72a747174/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/badmaarag-batjargal-72a747174/"}} />
           <Profile person={{name: "Nomundelger Amarbat",
                             imageId: anunu,
                             description: "Program Director",
-                            linkedIn: "https://www.linkedin.com/in/anomuka/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/anomuka/"}} />
           <Profile person={{name: "Khureltulga Tsolmon",
                             imageId: anunu,
                             description: "Professional Development Director",
-                            linkedIn: "https://www.linkedin.com/in/at3/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/at3/"}} />
           <Profile person={{name: "James Kitticha Edwards",
                             imageId: anunu,
                             description: "Website Dude",
-                            linkedIn: "https://www.linkedin.com/in/jkedwards8/"}}
-                   size={150}/>
+                            linkedIn: "https://www.linkedin.com/in/jkedwards8/"}} />
         </div>
       </div>
     );
