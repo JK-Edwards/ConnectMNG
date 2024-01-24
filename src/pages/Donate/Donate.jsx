be6519669c508c5855fdf5720a83d4b5710cf6da
jkedwards <jamese8@uw.edu>
2024-01-23 16:51:58 -0800

    Implemented donation page

--- src/pages/Donate/Donate.jsx
+++ src/pages/Donate/Donate.jsx
@@ -1,13 +1,47 @@
 import React from "react";
 
+import { useState } from "react";
+
 import "./Donate.css";
 
 export default function Donate() {
+	const cashAmounts = ["$10", "$20", "$50", "$100", "$250", "$500"];
+	const [donateAmount, updateDonateAmount] = useState("");
+
+	const handleInputChange = (event) => {
+		updateDonateAmount(event.target.value);
+	  };
+
 	return (
 		<div className={"donateContainer"}>
 			<h1>
-				This is the donation page!
+				EMPOWER YOUNG MONGOLIAN PROFESSIONALS
 			</h1>
+			<h2>
+				DONATE TO CONNECT MNG
+			</h2>
+			<h3>
+				MAKE A ONE-TIME GIFT
+			</h3>
+			<div className={"moneyButtonContainer"}>
+				{cashAmounts.map((amount, index) =>
+					<MoneyButton key={index} text={amount} onClick={() => updateDonateAmount(amount)} />
+				)}
+			</div>
+			<input className={"donateInput"}
+				   placeholder={"Custom Amount"}
+				   onChange={handleInputChange}
+				   value={donateAmount}
+			/>
+			<button className={"donateButton"}>DONATE</button>
 		</div>
+	);
+};
+
+function MoneyButton({text, onClick}) {
+	return (
+		<button className={"moneyButton"} onClick={onClick}>
+			{text}
+		</button>
 	);
 };
