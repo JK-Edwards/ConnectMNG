d94b988026b47f52888fafddb5e050de933d968a
jkedwards <jamese8@uw.edu>
2024-02-16 16:07:27 +0700

    Added new background image and moved page padding to contentContainer

--- src/App.jsx
+++ src/App.jsx
@@ -37,23 +37,25 @@ export default function App() {
 						<Header isEnglish={isEnglish} setIsEnglish={setIsEnglish} />
 						<Navigation />
 					</div>
-					<Routes>
-						<Route path="*" element={<Home />} />
-						<Route path="/programs" element={<Programs />} />
-						<Route path="/programs/career-development-program" element={<CareerDevelopment />} />
-						<Route path="/programs/mentorship-program" element={<MentorshipProgram />} />
-						<Route path="/programs/networking-program" element={<NetworkingProgram />} />
-						<Route path="/get-involved/volunteer" element={<Volunteer />} />
-						<Route path="/get-involved/membership" element={<Membership />} />
-						<Route path="/resources/podcasts" element={<Podcasts />} />
-						<Route path="/events" element={<Events />} />
-						<Route path="/resources/blogs" element={<Blogs />} />
-						<Route path="/resources/blogs/resume-blog" element={<ResumeBlog />} />
-						<Route path="/about-us" element={<About />} />
-						<Route path="/resources" element={<Resources />} />
-						<Route path="/contact-us" element={<Contact />} />
-						<Route path="/donate" element={<Donate />} />
-					</Routes>
+					<div className={"pageContainer"}>
+						<Routes>
+							<Route path="*" element={<Home />} />
+							<Route path="/programs" element={<Programs />} />
+							<Route path="/programs/career-development-program" element={<CareerDevelopment />} />
+							<Route path="/programs/mentorship-program" element={<MentorshipProgram />} />
+							<Route path="/programs/networking-program" element={<NetworkingProgram />} />
+							<Route path="/get-involved/volunteer" element={<Volunteer />} />
+							<Route path="/get-involved/membership" element={<Membership />} />
+							<Route path="/resources/podcasts" element={<Podcasts />} />
+							<Route path="/events" element={<Events />} />
+							<Route path="/resources/blogs" element={<Blogs />} />
+							<Route path="/resources/blogs/resume-blog" element={<ResumeBlog />} />
+							<Route path="/about-us" element={<About />} />
+							<Route path="/resources" element={<Resources />} />
+							<Route path="/contact-us" element={<Contact />} />
+							<Route path="/donate" element={<Donate />} />
+						</Routes>
+					</div>
 					<div className={"pinnedSupportPopup"}>
 						<SupportPopup />
 					</div>
