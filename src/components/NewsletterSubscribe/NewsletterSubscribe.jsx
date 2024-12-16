d7e04d680162cce7088ab7cdabde83fc1daf80a0
AnuujinChadraa <anuujinchadraa@gmail.com>
2024-12-15 16:55:00 -0800

    message

--- src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
+++ src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
@@ -25,6 +25,27 @@ export default function NewsletterSubscribe() {
             return;
         }
 
+        const handleSubmit = (e) => {
+            const form = {
+                Email: e.target.email.value
+            }
+            console.log(form);
+    
+            emailjs.send('service_ykb70ma', 'template_97jazd7', form, 'xAIFrT-0HrsUOzt1d')
+            .then(res => {
+                return res
+            })
+            .then(data => {
+                console.log("data from response", JSON.stringify(data));
+                if(data.status <299){
+                    e.target.email.value = "";
+                }
+            })
+            .catch(error => {
+                console.log("ERROR \n " + JSON.stringify(error));
+            })
+        }
+
         localStorage.setItem("subscribedEmail", email);
         Swal.fire({
             title: "Good job!",
@@ -34,12 +55,12 @@ export default function NewsletterSubscribe() {
         //   alert("Subscribed with email: " + email);
 
 
-            emailjs.send('service_ykb70ma', 'template_97jazd7', templateParams)
-                .then(function(response) {
-                  console.log('SUCCESS!', response.status, response.text);
-                }, function(error) {
-                console.log('FAILED...', error);
-             });
+            // emailjs.send('service_ykb70ma', 'template_97jazd7', templateParams)
+            //     .then(function(response) {
+            //       console.log('SUCCESS!', response.status, response.text);
+            //     }, function(error) {
+            //     console.log('FAILED...', error);
+            //  });
         
     }  
     
