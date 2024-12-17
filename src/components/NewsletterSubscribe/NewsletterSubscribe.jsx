96e6282fff9d83c85e2f12c7fcbd5aa8e8f8617f
AnuujinChadraa <anuujinchadraa@gmail.com>
2024-12-17 12:38:21 -0800

    working email validation

--- src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
+++ src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
@@ -17,14 +17,37 @@ export default function NewsletterSubscribe() {
     };
 
 
-
-    function handleSubscribe() {
+    const handleSubscribe = (e) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         // Change to database when moving to production
-        if (email === "" || !emailRegex) {
+        if (email === "" || !emailRegex.test(email)) {
             alert("Please enter a valid email");
             return;
         }
+
+        const handleSubmit = (e) => {
+            const form = {
+                Email: email
+            }
+            console.log(form);
+    
+            emailjs.send('service_ykb70ma', 'template_97jazd7', form, 'xAIFrT-0HrsUOzt1d')
+            .then(res => {
+                return res
+            })
+            .then(data => {
+                console.log("data from response", JSON.stringify(data));
+                if(data.status <299){
+                    e.target.email.value = "";
+                }
+            })
+            .catch(error => {
+                console.log("ERROR \n " + JSON.stringify(error));
+            })
+        }
+
+        handleSubmit(e)
+
 
         localStorage.setItem("subscribedEmail", email);
         Swal.fire({
@@ -35,12 +58,12 @@ export default function NewsletterSubscribe() {
         //   alert("Subscribed with email: " + email);
 
 
-            emailjs.send('service_ykb70ma', 'template_97jazd7', templateParams)
-                .then(function(response) {
-                  console.log('SUCCESS!', response.status, response.text);
-                }, function(error) {
-                console.log('FAILED...', error);
-             });
+            // emailjs.send('service_ykb70ma', 'template_97jazd7', templateParams)
+            //     .then(function(response) {
+            //       console.log('SUCCESS!', response.status, response.text);
+            //     }, function(error) {
+            //     console.log('FAILED...', error);
+            //  });
         
     }  
     
