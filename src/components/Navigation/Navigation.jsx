c3630a6b44a0af030809fabbd5ce621cb45bf52a
Erkhemtugs <erhemevlee@gmail.com>
2025-01-11 12:41:04 -0600

    created separate mobile nav. Upon clicking, the dropdow menu disappeares.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -41,6 +41,8 @@ export default function Navigation() {
 		setShowActivities(false);
 		setShowResources(false);
 		setShowAbout(false);
+		const navContainer = document.querySelector('.mobileNavigationContainer');
+		navContainer.style.display = "none"; // Hide the menu
 	};
 
 	function handleDropdownSelect(selectedIndex) {
@@ -52,7 +54,7 @@ export default function Navigation() {
 
 	function navDropDown() {
 		if (window.innerWidth < 768) {
-			const navContainer = document.querySelector('.navigationContainer');
+			const navContainer = document.querySelector('.mobileNavigationContainer');
 			if (navContainer.style.display === "flex") {
 				navContainer.style.display = "none"; // Hide the menu
 			} else {
@@ -64,15 +66,79 @@ export default function Navigation() {
 	return (
 		<div className={"overallNavigationContainer"}>
 			<div className="mobile-nav-container">
-				<div className="mobile-logo-container">
-					<Link to="/" onClick={() => handleNavLinkSelect(0)}>
-						<img src={logo} alt={"Connect MNG Logo"} className={"logo"} />
-					</Link>
+				<div className="mobile-nav-no-dropdown">
+					<div className="mobile-logo-container">
+						<Link to="/" onClick={() => handleNavLinkSelect(0)}>
+							<img src={logo} alt={"Connect MNG Logo"} className={"logo"} />
+						</Link>
+					</div>
+					<div className="nav-dropdown-container">
+						<button className={"nav-dropdown"} onClick={navDropDown}>
+							<i className="fas fa-bars"></i>
+						</button>
+					</div>
 				</div>
-				<div className="nav-dropdown-container">
-					<button className={"nav-dropdown"} onClick={navDropDown}>
-						<i className="fas fa-bars"></i>
-					</button>
+				<div className={"mobileNavigationContainer"}>
+					<div>
+						<NavLink to={"/programs"}
+							text={intl.formatMessage({ id: "programs" })}
+							className={"button"}
+							isActive={activeIndex === 1}
+							onClick={() => handleNavLinkSelect(1)}
+						/>
+					</div>
+					<div>
+						<NavLink to={"/events"}
+							text={intl.formatMessage({ id: "events" })}
+							className={"button"}
+							isActive={activeIndex === 3}
+							onClick={() => handleNavLinkSelect(3)}
+						/>
+					</div>
+					<div>
+						<div className={"button"} onClick={handleAboutSelect}>
+							{intl.formatMessage({ id: "about" })}
+						</div>
+						{showAbout ?
+							<NavDropdown
+								onSelect={(selectedIndex) => handleDropdownSelect(selectedIndex)}
+								options={[intl.formatMessage({ id: "ourStory" }), intl.formatMessage({ id: "ourTeam" })]}
+								links={["/about-us/our-story", "/about-us/our-team"]}
+							/>
+							: null}
+					</div>
+					<div>
+						<div className={"button"} onClick={handleResourcesSelect}>
+							{intl.formatMessage({ id: "resources" })}
+						</div>
+						{showResources ?
+							<NavDropdown
+								onSelect={(selectedIndex) => handleDropdownSelect(selectedIndex)}
+								options={[intl.formatMessage({ id: "blogs" })]}
+								links={["/resources/blogs"]}
+							/>
+							: null}
+					</div>
+					<div>
+						<div className={"button"} onClick={handleVolunteerSelect}>
+							{intl.formatMessage({ id: "getInvolved" })}
+						</div>
+						{showActivities ?
+							<NavDropdown
+								onSelect={(selectedIndex) => handleDropdownSelect(selectedIndex)}
+								options={[intl.formatMessage({ id: "donate" }), intl.formatMessage({ id: "internships" }), intl.formatMessage({ id: "volunteer" })]}
+								links={["/get-involved/donate", "get-involved/internships", "/get-involved/volunteer"]}
+							/>
+							: null}
+					</div>
+					<div>
+						<NavLink to={"/contact-us"}
+							text={intl.formatMessage({ id: "contactUs" }).toUpperCase()}
+							className={"button contact"}
+							isActive={activeIndex === 6}
+							onClick={() => handleNavLinkSelect(6)}
+						/>
+					</div>
 				</div>
 			</div>
 			<div className={"navigationContainer"}>
