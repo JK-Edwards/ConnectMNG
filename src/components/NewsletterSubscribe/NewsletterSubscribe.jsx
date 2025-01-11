168c483ed4abb6d0bf3222c3fa5a338e57b48450
Erkhemtugs <erhemevlee@gmail.com>
2025-01-10 21:07:52 -0600

    Fixed emailjs subscription logic. Tested with test@test.com. Sends as it should.

--- src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
+++ src/components/NewsletterSubscribe/NewsletterSubscribe.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-
 import { useState } from "react";
-
 import "./NewsletterSubscribe.css";
-
 import emailjs from "emailjs-com";
-
 import Swal from "sweetalert2";
-
-
 export default function NewsletterSubscribe() {
     const [email, setEmail] = useState("");
-
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
-
-
     const handleSubscribe = (e) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         // Change to database when moving to production
@@ -24,46 +15,35 @@ export default function NewsletterSubscribe() {
             alert("Please enter a valid email");
             return;
         }
-
         const handleSubmit = (e) => {
             const form = {
-
                 to_name:"ConnectMNG",
                 from_email: email,
                 Email: email
             }
-    
             emailjs.send('service_g082yv8', 'template_bh9ivpm', form, 'O6pJDJsUT4I2tB8Xs')
-                Email: email
-            }
-            .then(res => {
-                return res
-            })
-            .then(data => {
-                if(data.status <299){
-                    e.target.reset();
-                }
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            })
         }
-
         handleSubmit(e)
-
-
         localStorage.setItem("subscribedEmail", email);
         Swal.fire({
             title: "Good job!",
             text: "Subscribed with email: " + email,
             icon: "success"
           });
-        
-    }  
-    
-    
-    
+        //   alert("Subscribed with email: " + email);
 
+
+            // emailjs.send('service_ykb70ma', 'template_97jazd7', templateParams)
+            //     .then(function(response) {
+            //       console.log('SUCCESS!', response.status, response.text);
+            //     }, function(error) {
+            //     console.log('FAILED...', error);
+            //  });
+
+    }  
+
+    
+    
     return (
         <div className="subscriptionContainer">
             <input
@@ -76,3 +56,4 @@ export default function NewsletterSubscribe() {
             <button onClick={handleSubscribe} className={"subscribeButton"}>Subscribe</button>
         </div>
     );
+}
