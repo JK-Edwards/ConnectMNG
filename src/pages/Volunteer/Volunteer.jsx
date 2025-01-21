b41bf264c974616f908fc612ab562a1f312d6712
Matthew <Matthewbalgan@gmail.com>
2025-01-21 14:19:26 -0800

    Volunteer page mobile friendly

--- src/pages/Volunteer/Volunteer.jsx
+++ src/pages/Volunteer/Volunteer.jsx
@@ -20,16 +20,48 @@ export default function Volunteer() {
 
 	return (
 		<div className={"volunteerContainer"}>
+			<div className={"volunteerGetInvolved only-mobile"}>
+				<h1>/ Get involved</h1>
+			</div>
 			<div className={'volunteerTitleContainer'}>
 				<h1>{intl.formatMessage({id: "volunteer"})}</h1>
 			</div>
 			<div className={'volunteerDescriptionContainer'}>
 				<p>{intl.formatMessage({id: "volunteerDesc"})}</p>
 			</div>
-			<div className={'volunteerCardContainer'}>
+			<div className={'volunteerCardContainer only-desktop'}>
 				<VolunteerCard flip={true} title={intl.formatMessage({id: "fundraising"})} desc={FUNDRAISING_CARD_DESC} link={fundraisingImage}/>
 				<VolunteerCard flip={false} title={intl.formatMessage({id: "events"})} desc={EVENTS_CARD_DESC} link={eventsImage}/>
 			</div>
+
+			<div className="thinSeparator only-mobile"></div>
+
+			<div className={'volunteerCardContainer only-mobile'}>
+				<div className="volunteerCard">
+					<h1>Website</h1>
+					<img
+						src={fundraisingImage}
+						alt="Website"
+						className="volunteerCardImageStyle"
+					/>
+					<p><i>Intern role description and responsibilities</i></p>
+					<div className="thinSeparator"></div>
+				</div>
+			</div>
+
+			<div className={'volunteerCardContainer only-mobile'}>
+				<div className="volunteerCard">
+					<h1>Program</h1>
+					<img
+						src={eventsImage}
+						alt="Website"
+						className="volunteerCardImageStyle"
+					/>
+					<p><i>Intern role description and responsibilities</i></p>
+					<div className="thinSeparator"></div>
+				</div>
+			</div>
+
 			<div className={'volunteerFormTitleContainer'}>
 				Small paragraph about the volunteer process
 			</div>
