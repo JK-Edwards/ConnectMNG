73e6a8bba385d47619dc1ca192c9d1b2b20f8303
Matthew <Matthewbalgan@gmail.com>
2025-02-04 16:21:40 -0800

    homepage mobile friendly

--- src/components/Carousel/ReactSlickCarousel.jsx
+++ src/components/Carousel/ReactSlickCarousel.jsx
@@ -5,11 +5,14 @@ import './ReactSlickCarousel.css'
 
 export default function SimpleSlider({slides}) {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 750,
     slidesToShow: 3,
     slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
   return (
     <Slider {...settings}>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -41,10 +41,32 @@ export default function Home() {
 			</div>
 		</Link>,
 	];
+	const blogSlidesMobile = [
+		<Link to={"/resources/blogs"}>
+			<div className={"carouselSlide"}>
+				<img src={slide1} height={850} width={850} alt={"Slide 1"} />
+			</div>
+		</Link>,
+		<Link to={"/resources/podcasts"}>
+			<div className={"carouselSlide"}>
+				<img src={slide2} height={850} width={850} alt={"Slide 2"} />
+			</div>
+		</Link>,
+		<Link to={"/resources/podcasts"}>
+			<div className={"carouselSlide"}>
+				<img src={slide2} height={850} width={850} alt={"Slide 2"} />
+			</div>
+		</Link>,
+		<Link to={"/resources/podcasts"}>
+			<div className={"carouselSlide"}>
+				<img src={slide2} height={850} width={850} alt={"Slide 2"} />
+			</div>
+		</Link>,
+	];
 
 	return (
 		<div className={"homeContainer"}>
-			<div>
+			<div className="only-desktop">
 				<div className={"missionTextContainer"}>
 					<h1>{intl.formatMessage({ id: "missionAndImpact" }).toUpperCase()}</h1>
 					<p>{intl.formatMessage({ id: "mission" })}</p>
@@ -74,6 +96,38 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
+
+			<div className={"homeContainer only-mobile"}>
+				<div className={"carouselContainer"}>
+						{/* <ItemCarousel slides={blogSlides} interval={2500}/> */}
+						<SimpleSlider slides={blogSlides} />
+				</div>
+				<div className={"missionTextContainer"}>
+					<h1>{intl.formatMessage({ id: "missionAndImpact" }).toUpperCase()}</h1>
+					<p>{intl.formatMessage({ id: "mission" })}</p>
+				</div>
+				<div className={"programGalleryContainer"}>
+					<h1>{intl.formatMessage({ id: "What We Offer" }).toUpperCase()}</h1>
+					<div className={"programGallery"}>
+						<div className="program">
+							<img className={"programImage"} src={prog1} alt={"Resources"}></img>
+							<h3>{intl.formatMessage({ id: "resources" })}</h3>
+							<StyledLink text={intl.formatMessage({ id: "learnMore" })} link={"/resources/blogs"} />
+						</div>
+						<div className="program">
+							<img className={"programImage"} src={prog2} alt={"Programs"}></img>
+							<h3>{intl.formatMessage({ id: "programs" })}</h3>
+							<StyledLink text={intl.formatMessage({ id: "learnMore" })} link={"/programs"} />
+						</div>
+						<div className="program">
+							<img className={"programImage"} src={prog3} alt={"Events"}></img>
+							<h3>{intl.formatMessage({ id: "events" })}</h3>
+							<StyledLink className={"link"} text={intl.formatMessage({ id: "learnMore" })} link={"/events"} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+		
 	);
 };
