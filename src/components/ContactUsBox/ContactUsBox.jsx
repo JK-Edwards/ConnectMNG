62fe06e784320c1f05b12039ebf1ed3d88363c9a
jason yeung <jasonyeung@MacBookPro.lan>
2025-05-13 00:19:01 -0500

    comment out fake phone number

--- src/components/ContactUsBox/ContactUsBox.jsx
+++ src/components/ContactUsBox/ContactUsBox.jsx
@@ -12,20 +12,22 @@ export default function ContactUsBox() {
                     {intl.formatMessage({ id: "contactUs" }).toUpperCase()}
                 </div>
             </div>
-            <div className={"contactInfo"}>
+           <div className={"contactInfo"}>
                 <div>
                     {intl.formatMessage({ id: "email" })}
                 </div>
                 <p>
                     connectmongol.mng@gmail.com
                 </p>
+                {/* 
                 <div>
-                    Phone
+                Phone 
                 </div>
                 <p>
                     206.123.4567
                 </p>
-            </div>
+                */}
         </div>
+        </div>  
     )
 };
